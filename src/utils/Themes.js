66bb2b4f35938967e268934c52fc7740ffafd7a5
Swapnil252383 <swapnil.rupnar_comp22@pccoer.in>
2024-07-27 21:11:05 +0530

    first commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#dde810",
+    primary:"#ede432",
+    text_primary:"#ff0505",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#cff705",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#ede432",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
